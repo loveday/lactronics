d798ec53c8a5399548b5d5b712af63a86939fc7e
Loveday <loveday.anyim@student.ncirl.ie>
2012-06-27 21:18:08 +0100

    message

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lactronics::Application.config.secret_token = 'f967805500e42e88e7d411a1ac8115cc9abe91fddaa81fd21be5ff6ca08386dddba57867b3f838b1c97bf3c557912929286b58b7eb6e559bb3f3762aec5e0721'
